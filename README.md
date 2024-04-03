b85385e5602cd41aecce9a6a6270b6dbb3db6b42
tappoy <tappoy1234@gmail.com>
2024-04-03 16:09:55 +0900

    add README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+# About
+This module provies convinient functions to access the environment variables.
+
+# Functions:
+- GetEnvString(key string, defaultValue string) string
+- GetEnvInt(key string, defaultValue int) int
--- env.go
+++ env.go
@@ -1,8 +1,3 @@
-// This module provies convinient functions to access the environment variables.
-// Functions:
-// - GetEnvString(key string, defaultValue string) string
-// - GetEnvInt(key string, defaultValue int) int
-
 package env
 
 import (
